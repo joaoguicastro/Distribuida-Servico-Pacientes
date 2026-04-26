d119c99fe09434ac31a3645e94ebcbb1643abb84
Davziinn <mdavi6085@gmail.com>
2026-04-26 12:05:50 -0300

    comunicando com outros serviços

--- src/main/java/com/healthsys/pacientes/messaging/PacienteProducer.java
+++ src/main/java/com/healthsys/pacientes/messaging/PacienteProducer.java
@@ -2,18 +2,20 @@ package com.healthsys.pacientes.messaging;
 
 import com.healthsys.pacientes.event.PacienteCriadoEvent;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PacienteProducer {
     private final RabbitTemplate rabbitTemplate;
 
-
-    public PacienteProducer(RabbitTemplate rabbitTemplate) {
+    public PacienteProducer(RabbitTemplate rabbitTemplate,
+                            Jackson2JsonMessageConverter messageConverter) {
         this.rabbitTemplate = rabbitTemplate;
+        this.rabbitTemplate.setMessageConverter(messageConverter);
     }
 
-    public void enviarEvento (PacienteCriadoEvent event) {
+    public void enviarEvento(PacienteCriadoEvent event) {
         rabbitTemplate.convertAndSend(
                 "paciente.exchange",
                 "paciente.criado",
